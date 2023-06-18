98d3833fd28bb2acd0e886997ba50d9d2b76f0a2
unknown <abooddablo@gmail.com>
2023-06-18 20:15:29 +0300

    working on ask.fm

--- app/Http/Controllers/AuthinticationController.php
+++ app/Http/Controllers/AuthinticationController.php
@@ -8,6 +8,16 @@ class AuthinticationController extends Controller
 {
     public function login()
     {
+
+
+        
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
     }
 
 
@@ -15,10 +25,26 @@ class AuthinticationController extends Controller
 
     public function register()
     {
+
+
+           User::create([
+
+
+            
+           ])
+   
+
+
     }
 
 
     public function logout()
     {
+
+
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
     }
 }
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -5,11 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel= 'stylesheet' href='app.css'>
 </head>
 <body>
     <form method="post">
-    <input type="text">
-    <input type="password">
+        <label>Login</label>
+    <input type="text" placeholder="E-mail or username">
+    <label>Password</label>
+    <input type="password" placeholder="Password">
+    <label>remember me</label>
+
     <input type="checkbox">
 <input type="submit" value="Log in">
 <div class="social_media">
